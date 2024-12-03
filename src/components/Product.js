27b86a9cbf2539bbd9a7ac9360e05c3078de7698
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-12-03 18:59:46 +0530

    product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import '../assets/CSS/layout.css';
+
 export default function Product(){
+    const [quantity, setQuantity] = useState(0)
+
+    const handleAddToCart = () => {
+        if (quantity > 0) {
+            props.addToCart({
+                name: props.name,
+                price: props.price * quantity,
+                qty: quantity,
+            })
+            setQuantity(0)
+        }
+    }
     
     return(
         <div className="grid-item">
 
             <div class="card">
-                <img  />
+                <img src={require(`../assets/image/${props.img}`)} />
                 <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
+                    <h5 class="card-title">{props.name}Price:{props.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+                        <input type="number" id="quantity" name="quantity" min={0} value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}/>
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={handleAddToCart}>Add to Cart</button>
                 </div>
             </div>
         </div>
